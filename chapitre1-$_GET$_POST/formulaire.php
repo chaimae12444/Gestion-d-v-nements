525a705e7b394e5f4992556f4103c8bc0b71c7e5
chaimae12444 <sedraoui.chaimae.solicode@gmail.com>
2026-04-06 12:43:20 +0100

    Add files via upload

---
+++ chapitre1-$_GET$_POST/formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="traitement.php" method="POST">
+    <label>NOM:</label>
+    <input type="text" name="nom">
+    <label>EMAIL:</label>
+    <input type="email" name="email">
+    <button type="submit">ENVOYER</button>
+
+    </form>
+</body>
+</html>
---
+++ chapitre1-$_GET$_POST/traitement.php
@@ -0,0 +1,18 @@
+<?php
+ require "formulaire.php";
+if($_SERVER["REQUEST_METHOD"]==="POST"){
+    $nom=trim($_POST['nom']);
+    $email=trim($_POST['email']);
+    if(empty($nom)||empty($email)){
+        echo "Tous les champs sont obligatoires.";
+    }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        echo"email invalide";
+    }else{
+        echo"bonjour $nom ,votre email $email.";
+    }
+
+    
+}
+?>
+
+
